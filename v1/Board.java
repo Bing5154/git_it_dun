00f90794522a532137a18495c2a0f06ae9398641
Bing5154 <bli7@stuy.edu>
2018-01-06 14:41:24 -0500

    board, not tested

--- v1/Board.java
+++ v1/Board.java
@@ -1,36 +1,42 @@
+import java.util.ArrayList;
+
 public class Board {
     private String whoseTurn;
     private final static int SIZE = 8;
-    private int Rleft;
-    private int Bleft;
-    private ArrayList<Checkers>[][] board;
+    private ArrayList<Arraylist<Checkers>> board;
+    private ArrayList<Checkers> red;
+    private ArrayList<Checkers> black;
 
 
     public Board() {
 	board = new String[SIZE][SIZE];
-	Bleft = 12;
-	Rleft = 12;
-	whoseTurn = "R";
+	red = new ArrayList <rCheckers>(12);
+	black = new ArrayList<Checkers>(12);	
+  	whoseTurn = "r";
 	
     }
 
     public void initBoard() {
+	//initialize all elements in the 2d array as Checkers
+	//Checkers should have a toString that represent them as "_"
 	for(int i = 0; i < SIZE; i++) {
 	    for(int j = 0; j < SIZE; j++) {
-		board[i][j] = "_";
+		board.get(i).set(j, new Checkers());
 	    }
 	}
 
+	//after setting all elements into Checkers
+	//set specific items of the arraylist into black checkers and red checkers
 	for(int j = 1; j < SIZE; j+=2) {
-	    board[1][j] = "B";
-	    board[5][j] = "R";
-	    board[7][j] = "R";	
+	    board.get(1).set(j,new bCheckers());
+	    board.get(5).set(j,new rCheckers());
+	    board.get(7).set(j,new rCheckers());
 	}
 
 	for(int j = 0; j < SIZE; j+=2) {
-	    board[0][j] = "B";
-	    board[2][j] = "B";
-	    board[6][j] = "R";
+	    board.get(0).set(j,new bCheckers());
+	    board.get(2).set(j,new bCheckers());
+	    board.get(6).set(j,new rCheckers());
 	}
     }
 
@@ -39,22 +45,10 @@ public class Board {
 	for(int i = 0; i < SIZE; i++) {
 	    System.out.print(i + " ");
 	    for(int j = 0; j < SIZE; j++) {
-		System.out.print(board[i][j] + " ");
+		System.out.print(board.get(i).get(j).toString() + "  ");	
 	    }
 	    System.out.println();
 	}
-    }
-
-    public int getBLeft() {
-	return Bleft;	
-    }
-
-    public int getRLeft() {
-	return Rleft;
-    }
-
-    public String getWhoTurn() {
-	return whoseTurn;
     }
 
     public static void main(String args[]) {
