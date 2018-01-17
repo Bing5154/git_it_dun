83997342ec46e24204e76b0712e915b8b26d0a5b
Bing5154 <bli7@stuy.edu>
2018-01-16 23:47:05 -0500

    added comments

--- v3/Board.java
+++ v3/Board.java
@@ -1,37 +1,42 @@
 import java.util.ArrayList;
 public class Board {
+    //private instance variables
     private char whoseTurn;
     private final static int SIZE = 8;
     private Checkers[][] board;
     private int rLeft = 12;
     private int bLeft = 12;
-    private String winner; 
+    //posr stores the row number of potential checkers availiable for forced capture
     private ArrayList<Integer> posr = new ArrayList<Integer>(12);
+    //posc stores the column number of potential checkers availiable for forced capture
     private ArrayList<Integer> posc = new ArrayList<Integer>(12);
+    //posd stores teh row number of potential checkers availiable for forced capture
     private ArrayList<String> posd = new ArrayList<String>(12);
 
-
-
+    //default constructor
     public Board() {
 	board = new Checkers[SIZE][SIZE];
 	whoseTurn = 'r';
-	winner = "Red";
 
     }
 
+    //reset posr after every single move, because positions have changed
     public void clearposr(){
 	posr = new ArrayList<Integer>(12);
 	
     }
 
+    //reset posc after every single move, because positions have changed
     public void clearposc(){
 	posc = new ArrayList<Integer>(12);
     }
 
+    //reset posd after every single move, because positions have changed
     public void clearposd() {
 	posd = new ArrayList<String>(12);
     }
 
+    //would be used in Woo to check if the checker has all properties availiable for forced capture
     public boolean checkF(int r, int c, String m) {
 	for(int i = 0; i < posr.size(); i++) {
 	    if(posr.get(i) == r && posc.get(i) == c && posd.get(i).equals(m)) {
@@ -40,54 +45,43 @@ public class Board {
 	}
 	return false;
     }
-    
+
+    //accessor of posr
     public ArrayList<Integer> getposr() {
 	return posr;
     }
 
+    //accessor for posc
     public ArrayList<Integer> getposc() {
 	return posc;
     }
 
+    //accessor for posd
     public ArrayList<String> getposd() {
 	return posd;
     }
 
-    public String getWinner() {
-	return winner;
-    }
-  
+    //accessor for number of red checkers left, helpful in end game
     public int getrLeft(){
 	return rLeft;
     }
 
+    //mutator for rLeft, substract 1 from total once jump is executed
     public void setrLeft(){
 	rLeft -= 1;
     }
 
-
+    //accessor for number of red checkers left, helpful in end game
     public int getbLeft(){
 	return bLeft;
     }
 
+    //mutator for bLeft, subtract 1 from total once jump is executed
     public void setbLeft() {
 	bLeft -= 1;
     }
 
-    public boolean isKing() {
-	boolean bol = false;
-	for (int c = 0; c < SIZE; c++) {
-	    if(board[0][c].getColor() == 'r') {
-		bol = true;
-	        winner = "Red";
-	    } else if (board[7][c].getColor() == 'b') {
-		bol = true;
-		winner = "Black";
-	    }
-	}
-	return bol;
-    }
-
+    
     public void initBoard() {
 	//initialize all elements in the 2d array as Checkers
 	//Checkers should have a toString that represent them as "_"
@@ -113,6 +107,7 @@ public class Board {
 	}
     }
 
+    //the toString method of board
     public void printBoard() {
 	System.out.println("  0  1  2  3  4  5  6  7 ");
 	for(int i = 0; i < SIZE; i++) {
@@ -124,8 +119,10 @@ public class Board {
 	}
     }
 
+    //check if regular forward left movement is valid
     public boolean flValid(int r, int c){
 	boolean bol = true;
+	//in range?
 	if ( r == 0 || c == 0) {
 	    bol = false;
 	} else if(board[r-1][c-1].getColor() != '_') {
@@ -136,18 +133,25 @@ public class Board {
 	return bol;
     }
 
+    //check if regular forward right movement is valid
     public boolean frValid(int r, int c) {
 	boolean bol = true;
+	//in range?
 	if ( r == 0 || c == 7) {
 	    bol = false;
-	} else if(board[r-1][c+1].getColor() != '_') {
+	}
+	//not blocked?
+	else if(board[r-1][c+1].getColor() != '_') {
 	    bol =  false;
-	} else if (board[r][c] instanceof BChecker){
+	}
+	//your side?
+	else if (board[r][c] instanceof BChecker){
 	    bol = false;
         }
 	return bol;
     }
 
+    //check if regular backward left movement is valid
     public boolean blValid(int r, int c) {
 	boolean bol = true;
 	if ( r == 7 || c == 0) {
@@ -160,6 +164,7 @@ public class Board {
 	return bol;
     }
 
+    //check if regular backward right movement is valid
     public boolean brValid(int r, int c) {
 	boolean bol = true;
 	if ( r == 7 || c == 7) {
@@ -172,18 +177,21 @@ public class Board {
 	return bol;
     }
 
+    //execute forward left movement
     public void flMove(int x, int y) {
 	Checkers temp;
+	//if regular checker reaches the opposite end, turn into king
 	if (x - 1 == 0 && board[x][y].getColor() == 'r'){
 	    temp = new RKing();
 	} else{
 	    temp = board[x][y];
 	}
+	//swap
 	board[x][y] = board[x-1][y-1];
 	board[x-1][y-1] = temp; 
     }
         
-
+    //execute forward right movement
     public void frMove(int x, int y) {
 	Checkers temp;
 	if (x -1 == 0 && board[x][y].getColor() == 'r'){
@@ -195,6 +203,7 @@ public class Board {
 	board[x-1][y+1] = temp;
     }
 
+    //execute backward right movement
     public void brMove(int x, int y) {
 	Checkers temp;
 	if(x + 1 == 7 && board[x][y].getColor() == 'b'){
@@ -206,6 +215,7 @@ public class Board {
 	board[x+1][y+1] = temp;
     }
 
+    //execute backward left movement
     public void blMove(int x, int y) {
 	Checkers temp;
 	if(x + 1 == 7 && board[x][y].getColor() == 'b'){
@@ -217,6 +227,7 @@ public class Board {
 	board[x+1][y-1] = temp;
     }
 
+    //check if forward left jump is valid
     public boolean fljumpValid(int x, int y)  {
         boolean bol = false;
         char c = board[x][y].getColor();
@@ -229,6 +240,7 @@ public class Board {
         return bol;
     }
 
+    //check if forward right jump is valid
     public boolean frjumpValid(int x, int y) {
         boolean bol = false;
         char c = board[x][y].getColor();
@@ -241,7 +253,7 @@ public class Board {
         return bol;
     }
 
-
+    //check if backward right jump is valid
     public boolean brjumpValid(int x, int y){
         boolean bol = false;
         char c = board[x][y].getColor();
@@ -254,6 +266,7 @@ public class Board {
         return bol;
     }
 
+    //check if backward left jump is valid
     public boolean bljumpValid(int x, int y){
         boolean bol = false;
         char c = board[x][y].getColor();
@@ -267,21 +280,27 @@ public class Board {
     }
 
     
-
+    //check for forced capture
     public boolean forcedCapture(int row, int col, char comp){
         boolean boo = false;
+	//loop through the entire checkerboard --> kinda insufficient, but don't know alternative ways to do it
         for(int r = 0; r < SIZE; r++) {
             for (int c = 0; c < SIZE; c++) {
+		//special check for the Kings
                 if(board[r][c].getColor() == comp) {
 		    if (!(board[r][c] instanceof BChecker) && board[r][c].getColor() != '_') {
+			//checkers in this area (restrictions below) could only move fr
 			if (c < 2 && r > 1) {
 			    if (frjumpValid(r, c)) {
+				//remember the properties of checkers availiable for forced capture
+				//so that it can be checked against user input 
 				posr.add(r);
 				posc.add(c);
 				posd.add("fr");
 				boo = true;
 			    }
 			}
+			//checkers in this area (restriction below) could only move fl
 			if (c > 5 && r > 1) {
 			    if (fljumpValid(r, c)) {
 				posr.add(r);
@@ -290,6 +309,7 @@ public class Board {
 				boo = true;
 			    }
 			}
+			//checkers in this area (restriction below) could move fl and fr
 			if(r > 1 && c >= 2 && c <= 5){
 			    if (fljumpValid(r, c)) {
 				posr.add(r);
@@ -308,12 +328,14 @@ public class Board {
 		    if (!(board[r][c] instanceof RChecker) && board[r][c].getColor() != '_') {
 			if (c < 2 && r < 6) {
 			    if (brjumpValid(r, c)) {
+				//checkers in this area (restriction below) could move br
 				posr.add(r);
 				posc.add(c);
 				posd.add("br");
 				boo = true;
 			    }
 			}
+			//checkers in this area (restriction below) could only move bl
 			if (c > 5 && r < 6) {
 			    if (bljumpValid(r, c)) {
 				posr.add(r);
@@ -322,6 +344,7 @@ public class Board {
 				boo = true;
 			    }
 			}
+			//checkers in this area (restriction below) could only move br and bl
 			if (r < 6 && c >= 2 && c <= 5) {
 			    if (brjumpValid(r, c)) {
 				posr.add(r);
@@ -344,10 +367,11 @@ public class Board {
     }
 
 
-
+    //execute forward left jump
     public void flJump(int x, int y){
 	Checkers space = new Checkers();
 	Checkers temp;
+	//turn into a king when jump into the last row on the opposite side
 	if (x - 2 == 0 && board[x][y].getColor() == 'r') {
 	    temp = new RKing();
 	} else {
@@ -360,10 +384,11 @@ public class Board {
     }
 
 
-
+    //execute forward right jump
     public void frJump(int x, int y){
 	Checkers space = new Checkers();
 	Checkers temp;
+	//turn into king
 	if( x - 2 == 0 && board[x][y].getColor() == 'r'){
 	    temp = new RKing();
 	}else {
@@ -376,6 +401,7 @@ public class Board {
 	
     }
 
+    //execute backward left jump
     public void blJump(int x, int y){
         Checkers space = new Checkers();
         Checkers temp;
@@ -390,6 +416,7 @@ public class Board {
 	setrLeft();
     }
 
+    //execute backward right jump
     public void brJump(int x, int y){
         Checkers space = new Checkers();
         Checkers temp;
@@ -405,7 +432,7 @@ public class Board {
     }
 
 
-
+    //debug purpose, useful in validChecker
     public char boardCheckerColor(int row, int column) {
         if (board[row][column].getColor() == 'r') {
 	    /*debug
